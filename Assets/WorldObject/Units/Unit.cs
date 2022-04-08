0b3f1774f4a10142455ad362fe4b77337d61a522
Theemiss <midinfotn401@gmail.com>
2022-04-08 02:03:45 +0100

    Created Unit and Building Selector , HUD , and Camera Rotation

---
+++ Assets/WorldObject/Units/Unit.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : WorldObject
+{
+    // Start is called before the first frame update
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    protected override void OnGUI()
+    {
+        base.OnGUI();
+    }
+}
